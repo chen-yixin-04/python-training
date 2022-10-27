524adef152738aba92dbcdc05fd3defc0b8cfc2a
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-10-27 10:14:02 +0000

    versione01

---
+++ main_5.py
@@ -0,0 +1,5 @@
+# Python non ha una random()funzione per creare un numero casuale, ma Python ha un modulo integrato chiamato randomche può essere utilizzato per creare numeri casuali:
+#Importa il modulo casuale e visualizza un numero casuale compreso tra 1 e 9:
+
+import random
+print(random.randrange(1,10))
