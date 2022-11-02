7879a56c9eb621fdc799ebfe20a6ba2260866bfe
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-02 20:24:24 +0000

    es2

--- prova.py
+++ prova.py
@@ -1,2 +1,7 @@
-thislist = ["apple", "banana", "cherry"]
-print(thislist)
+#Riempi due variabili (x e y) con dei valori. Se x è maggiore di y stampa “ciao”. Se x è minore o uguale a y stampa “arrivederci”
+x = 5
+y = 10
+if x>y:
+    print("ciao")
+else:
+    print("arrivederci")
