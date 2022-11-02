aecf628c66e1e56de1bfe1f9b3f5a4ad9a623b73
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-02 20:37:36 +0000

    es3

--- prova.py
+++ prova.py
@@ -1,3 +1,6 @@
+thislist = ["apple", "banana", "cherry"]
+ for x in thislist
+print(thislist)
 #Riempi due variabili (x e y) con dei valori. Se x è maggiore di y stampa “ciao”. Se x è minore o uguale a y stampa “arrivederci”
 x = 5
 y = 10
@@ -5,3 +8,11 @@ if x>y:
     print("ciao")
 else:
     print("arrivederci")
+
+#Riempi tre variabili (x, y ,z) con dei valori. 
+#Se x è maggiore di y e maggiore di z stampa ‘X è il numero maggiore’
+x=3
+y=6
+z=2
+if x>y and x>z:
+ print("X è il numero maggiore")
