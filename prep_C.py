4ffc83382a09f3944e699b05e8f91e4320dc6620
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-17 11:58:05 +0000

    versione01

---
+++ prep_C.py
@@ -0,0 +1,10 @@
+#Chiedi all’utente di inserire 5 numeri e stampa solo i numeri pari in ordine inverso
+numeri = []
+for z in range(5):
+    numero = int(input("inserisci numero" + str(z+1) + ": "))
+    numeri.append(numero)
+for x in numeri:
+ check=int(x/2)
+ if (check*2)=!x:
+  print(x)
+
