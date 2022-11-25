4911e67038b849c85b01fbcefc73ea8f1f0da7a9
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-25 20:30:07 +0000

    versione02

--- prep_B.py
+++ prep_B.py
@@ -2,9 +2,9 @@
 
 
 numeri = []
-for z in range(5):
-    numero = int(input("inserisci numero" + str(z+1) + ": "))
-    numeri.append(numero)
-
+for x in range(5):
+    n = int(input("inserisci numero" + str(x+1) + ": "))
+    numeri.append(n)
 print(numeri)
-print(numeri.reverse())
+numeri.reverse()
+print(numeri)
