946a6d6a9ccd1a70a96f2ddae126502a3dc39284
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-26 12:44:31 +0000

    versione03

--- prep_A.py
+++ prep_A.py
@@ -15,17 +15,34 @@
 #print(numeri)
 #print("..................................................................")
 
-#Chiedi all’utente di inserire 5 numeri e stampa solo i numeri pari in ordine inverso
+#Chiedi all’utente di inserire 5 numeri e stampa solo i numeri pari in ordine inverso e contali
 
-lista = []
-for y in range(5):
-  l = int(input("inserisci numero"+" "+str(y+1)+" : "))
-  check = int(l/2)
-  if (check*2)== l:
-    lista.append(l)
-print(lista)
-lista.reverse()
-print(lista)
+#lista = []
+#count=0
+#for y in range(5):
+#  l = int(input("inserisci numero"+" "+str(y+1)+" : "))
+#  check = int(l/2)
+ # if (check*2)== l:
+ #   lista.append(l)
+ #   count=count+1
+#print(lista)
+#lista.reverse()
+#print(lista,count)
+
+#Chiedi all’utente quanti numeri vuole inserire. 
+#Leggi tutti i numeri in input. Stampa tutti i numeri inseriti al quadrato
+
+lista=[]
+quadrato=[]
+y=int(input("quanti numeri vuoi inserire? "))
+for x in range(y):
+  z=int(input("inserisci numero "+str(x+1)+": "))
+  lista.append(z)
+  q=z*z
+  quadrato.append(q)
+print(lista,quadrato)
+ 
+
 
 
 
