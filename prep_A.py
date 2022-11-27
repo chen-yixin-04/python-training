bd771bca7def4cc62602c6fbdaf527e6e69d6ec3
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-27 20:54:52 +0000

    v1

--- prep_A.py
+++ prep_A.py
@@ -1,64 +1,11 @@
 #Chiedi all’utente di inserire 5 numeri e stampa i numeri man mano che vengono inseriti
 
-#for x in range(5):
-#    n=int(input("inserisci numero"+" "+str(x+1)+" : "))
-#    print(n)
-#print("..................................................................")
-
-#Chiedi all’utente di inserire 5 numeri e stampa i numeri in ordine inverso
-#numeri = []
-#for z in range(5):
-#    num = int(input("inserisci numero"+" "+str(x+1)+" : "))
-#    numeri.append(num)
-#print(numeri)
-#numeri.reverse()
-#print(numeri)
-#print("..................................................................")
-
-#Chiedi all’utente di inserire 5 numeri e stampa solo i numeri pari in ordine inverso e contali
-
-#lista = []
-#count=0
-#for y in range(5):
-#  l = int(input("inserisci numero"+" "+str(y+1)+" : "))
-#  check = int(l/2)
- # if (check*2)== l:
- #   lista.append(l)
- #   count=count+1
-#print(lista)
-#lista.reverse()
-#print(lista,count)
-
-#Chiedi all’utente quanti numeri vuole inserire. 
-#Leggi tutti i numeri in input. Stampa tutti i numeri inseriti al quadrato
-
-#lista=[]
-#quadrato=[]
-#y=int(input("quanti numeri vuoi inserire? "))
-#for x in range(y):
- # z=int(input("inserisci numero "+str(x+1)+": "))
-  #lista.append(z)
- # q=z*z
-  #quadrato.append(q)
-#print(lista,quadrato)
-
-#Definisci una funzione chiamata ‘primo’ che riceva come parametro un numero intero e ritorni true in caso il numero fosse primo
-
-#def primo(n):
-  #for x in range(2,n):
-    #check=int(n/x)
-#y = int(input("inserisci numero"))
-#x=primo(y)
-#print(x)
+for x in range(5):
+    n=int(input("inserisci numero"+" "+str(x+1)+" : "))
+    print(n)
 
 
-def primo(n):
-  for x in range(2,n):
-    check=int(n/x)
-    if (check*x)!=n: return False
-  return True
-m=int(input("inserisci numero: "))
-x=primo(m)
+
 
 
 
