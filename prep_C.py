b86c65d2578eee64584689652cf5533b2c58b59b
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-27 20:48:33 +0000

    v1

--- prep_C.py
+++ prep_C.py
@@ -5,6 +5,22 @@ for z in range(5):
     numeri.append(numero)
 for x in numeri:
  check=int(x/2)
- if (check*2)=!x:
+ if (check*2)!=x:
   print(x)
+
+print("...........................................................")
+
+#Chiedi all’utente di inserire 5 numeri e stampa solo i numeri pari in ordine inverso e contali
+
+lista = []
+count=0
+for y in range(5):
+  l = int(input("inserisci numero"+" "+str(y+1)+" : "))
+  check = int(l/2)
+  if (check*2)== l:
+    lista.append(l)
+    count=count+1
+print(lista)
+lista.reverse()
+print(lista,count)
 
