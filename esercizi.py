2a49f85bb28e8be76bebfcde93b4fb9101de445e
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-27 19:35:35 +0000

    v1

--- esercizi.py
+++ esercizi.py
@@ -1,8 +1,56 @@
-def primo():
- n=int(input("Inserisci un numero naturale n maggiore di 1: "))
- primo=True # ipotizziamo che N sia primo
- for i in range(2,n): # test di divisibilità
-  if n%i==0:return False
- if primo: return False
- else: return False
-primo()
+#Chiedi all’utente di inserire 5 numeri e stampa i numeri man mano che vengono inseriti
+
+for x in range(5):
+    n=int(input("inserisci numero"+" "+str(x+1)+" : "))
+    print(n)
+print("..................................................................")
+
+#Chiedi all’utente di inserire 5 numeri e stampa i numeri in ordine inverso
+numeri = []
+for z in range(5):
+    num = int(input("inserisci numero"+" "+str(z+1)+" : "))
+    numeri.append(num)
+print(numeri)
+numeri.reverse()
+print(numeri)
+print("..................................................................")
+
+#Chiedi all’utente di inserire 5 numeri e stampa solo i numeri pari in ordine inverso e contali
+
+lista = []
+count=0
+for y in range(5):
+  l = int(input("inserisci numero"+" "+str(y+1)+" : "))
+  check = int(l/2)
+  if (check*2)== l:
+    lista.append(l)
+    count=count+1
+print(lista)
+lista.reverse()
+print(lista,count)
+
+#Chiedi all’utente quanti numeri vuole inserire. 
+#Leggi tutti i numeri in input. Stampa tutti i numeri inseriti al quadrato
+
+lista=[]
+quadrato=[]
+y=int(input("quanti numeri vuoi inserire? "))
+for x in range(y):
+  z=int(input("inserisci numero "+str(x+1)+": "))
+  lista.append(z)
+  q=z*z
+  quadrato.append(q)
+print(lista,quadrato)
+
+#Definisci una funzione chiamata ‘primo’ che riceva come parametro un numero intero e ritorni true in caso il numero fosse primo
+
+
+def primo(n):
+  for x in range(2,n):
+    check=int(n/x)
+    if (check*x)!=n: return False
+  return True
+m=int(input("inserisci numero: "))
+l=primo(m)
+print(l)
+
