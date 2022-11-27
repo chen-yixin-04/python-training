b86c03881a072871aeb129867e58c63ac7d4f40d
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-27 21:34:42 +0000

    v1

--- prep_F.py
+++ prep_F.py
@@ -1,9 +1,24 @@
-def primo(n):
-    for x in range(2,n):
-        check=int(n/x)
-        if check * x != n: return("primo")
-    return("non primo")
+#Scrivi una funzione che, data una lista di numeri in input, fornisce in output un istogramma basato su questi numeri, usando asterischi per disegnarlo.
+#Data ad esempio la lista [3, 7, 9, 5], la funzione dovrà produrre questa sequenza:
+#***
+#*******
+#*********
+#*****
 
-z=int(input("inserisci numero: "))
-y=primo(z)
-print(y)
+x = input("quanti numeri vuoi inserire? ")
+x = int(x)
+lista=[]
+for y in range(x):
+    z = int(input("inserisci numero: "))
+    lista.append(z)
+for n in lista:
+    print("*"*n)
+print(".....................................................")
+
+def funzione(n):
+ for x in range(1):
+    print("*"*n)
+funzione(3)
+funzione(7)
+funzione(9)
+funzione(5)
