59d680ac3c7d19ce28993f7f919c63c4f8d13df4
chen-yixin-04 <chen.yi.xin@iisgalvanimi.edu.it>
2022-11-30 12:00:33 +0000

    v1

---
+++ prep_J.py
@@ -0,0 +1,16 @@
+#Definisci una funzione che calcoli il Massimo comun divisore tra una lista di numeri ricevuta in input
+import math
+def funzione(lista):
+   print(lista)
+   mcd=math.gcd(*n)
+
+
+
+
+lista=[]
+y=int(input("quanti numeri vuoi inserire? "))
+for x in range(y):
+    n=int(input("dimmi un numero:"))
+    lista.append(n)
+
+funzione(lista)
